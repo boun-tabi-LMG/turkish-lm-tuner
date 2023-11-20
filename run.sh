158cec22fd967d155f3d6f7ecd9f7dd0c7379e48
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-20 11:58:49 +0300

    Add run script for ultramarine

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=ft-summ
+#SBATCH --mail-type=ALL
+#SBATCH --output=%j.log
+#SBATCH --mail-user=gokceuludogan@gmail.com
+#SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+
+source /opt/python3/venv/base/bin/activate
+
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+cd ~/t5-tuner
+pip install -r requirements.txt
+pip install wandb
+python src/finetune.py --config-name title_generation
+#python src/finetune.py --config-name summarization
