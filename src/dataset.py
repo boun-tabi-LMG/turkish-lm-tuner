92558da8c7020833712061d96a0cf0b731aeea59
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-28 22:27:42 +0300

    Fix compute_token_length

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
 import datasets
 from transformers import PreTrainedTokenizerFast
+import numpy as np
 from utils import (
     default_preprocess_function,
     preprocess_trnews_summarization,
@@ -57,7 +58,7 @@ dataset_mapping = {
 
 
 class DatasetProcessor:
-    def __init__(self, dataset_name, task, task_format, task_mode, tokenizer_name, max_input_length=128, max_target_length=128):
+    def __init__(self, dataset_name, task, task_format, task_mode, tokenizer_name, max_input_length, max_target_length):
         self.dataset_name = dataset_name
         self.task = task
         self.task_format = task_format
@@ -75,19 +76,38 @@ class DatasetProcessor:
         preprocess_function = self.get_preprocess_function()
         column_names = dataset.column_names
         processed_dataset = dataset.map(preprocess_function, remove_columns=column_names, batched=True)
-        max_input_length, max_target_length = self.calculate_max_input_length(processed_dataset)
-        print(f"Max input length: {max_input_length} Max target length: {max_target_length}")
+        if self.max_input_length == -1 or self.max_target_length == -1:
+            self.compute_token_length(processed_dataset)
         tokenized_dataset = processed_dataset.map(self.tokenize_function, batched=True)
         return tokenized_dataset
 
-    def calculate_max_input_length(self, dataset):
+    def compute_token_length(self, dataset):
 
-        def get_max_length(examples, field='input_text'):
-            return max(len(ex) for ex in self.tokenizer(examples[field]))
+        def get_max_length(examples):
+            return {
+                'input_len': [len(ex) for ex in self.tokenizer(examples['input_text'])['input_ids']],
+                'target_len': [len(ex) for ex in self.tokenizer(examples['target_text'])['input_ids']]
+            }
 
-        max_input_length = dataset.map(get_max_length, batched=True, batch_size=1000)
-        max_target_length = dataset.map(get_max_length, batched=True, batch_size=1000, field='target_text')
-        return max_input_length, max_target_length
+        dataset = dataset.map(get_max_length, batched=True, batch_size=8)
+        max_input_length = max([i['input_len'] for i in dataset])
+        max_target_length = max([i['target_len'] for i in dataset])
+        mean_input_length = sum([i['input_len'] for i in dataset])/len(dataset)
+        mean_target_length = sum([i['target_len'] for i in dataset])/len(dataset)
+        print(f"Mean input length: {mean_input_length} Mean target length: {mean_target_length}")
+        print(f"Max input length: {max_input_length} Max target length: {max_target_length}")    
+        input_percent90 = np.percentile([i['input_len'] for i in dataset], 90)
+        target_percent90 = np.percentile([i['target_len'] for i in dataset], 90)
+        print(f"90th percentile input length: {input_percent90} 90th percentile target length: {target_percent90}")
+        input_percent95 = np.percentile([i['input_len'] for i in dataset], 95)
+        target_percent95 = np.percentile([i['target_len'] for i in dataset], 95)
+        print(f"95th percentile input length: {input_percent95} 95th percentile target length: {target_percent95}")
+        input_percent99 = np.percentile([i['input_len'] for i in dataset], 99)
+        target_percent99 = np.percentile([i['target_len'] for i in dataset], 99)
+        print(f"99th percentile input length: {input_percent99} 99th percentile target length: {target_percent99}")
+        input_percent999 = np.percentile([i['input_len'] for i in dataset], 99.9)
+        target_percent999 = np.percentile([i['target_len'] for i in dataset], 99.9)
+        print(f"99.9th percentile input length: {input_percent999} 99.9th percentile target length: {target_percent999}")
     
     def get_preprocess_function(self):
         # Mapping of dataset_name and task to corresponding preprocess functions
