bb15c9677765d89ee38935260581a519cd13e042
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-28 22:02:26 +0300

    Fix preprocess_wikiann_ner

--- src/utils.py
+++ src/utils.py
@@ -68,39 +68,55 @@ def preprocess_mkqa_qg(examples):
     return {"input_text": answer, "target_text": query}
 
 def preprocess_wikiann_ner(examples):
-    tokens = examples['tokens']
-    spans = examples['spans']
-    for i, span in enumerate(spans):
-        spans[i] = span.replace('PER: ', 'Kişi: ').replace('LOC: ', 'Yer: ').replace('ORG: ', 'Kuruluş: ')
-    tag_type = ''
-    tag_dict = {}
-    for span in spans:
-        if span.startswith('Kişi: '):
-            tag_type = 'PERSON'
-        elif span.startswith('Yer: '):
-            tag_type = 'LOCATION'
-        elif span.startswith('Kuruluş: '):
-            tag_type = 'ORGANIZATION'
-        if tag_type not in tag_dict:
-            tag_dict[tag_type] = []
-        tag_dict[tag_type].append(span.replace('Kişi: ', '').replace('Yer: ', '').replace('Kuruluş: ', ''))
-    for tag_type in tag_dict:
-        new_l = []
-        for el in tag_dict[tag_type]:
-            if el not in new_l:
-                new_l.append(el)
-        tag_dict[tag_type] = new_l
-    input_text = ' '.join(tokens)
-    target_l = []
-    target_text = ''
-    for tag_type in tag_dict:
-        target_l.append(f'{tag_type}: {", ".join(tag_dict[tag_type])}')
-    target_text = ' | '.join(target_l)
-    target_text = target_text.replace('PERSON: ', 'Kişi: ').replace('LOCATION: ', 'Yer: ').replace('ORGANIZATION: ', 'Kuruluş: ').strip()
-    input_text = input_text.strip()
-    if not target_text:
-        target_text = 'Bulunamadı.'
-    return {'input_text': input_text, 'target_text': target_text}
+    input_texts = []
+    target_texts = []
+    for tokens, ner_tags in zip(examples['tokens'], examples['ner_tags']):
+        token_str, tag_type = '', ''
+        tag_dict = {}
+        for j, tag in enumerate(ner_tags):
+            if tag == 0:
+                if token_str:
+                    if tag_type not in tag_dict:
+                        tag_dict[tag_type] = []
+                    tag_dict[tag_type].append(token_str)
+                token_str, tag_type = '', ''
+            elif tag in [1, 3, 5]:
+                if token_str:
+                    if tag_type not in tag_dict:
+                        tag_dict[tag_type] = []
+                    tag_dict[tag_type].append(token_str)
+                if tag == 1:
+                    tag_type = 'PERSON'
+                elif tag == 3:
+                    tag_type = 'ORGANIZATION'
+                elif tag == 5:
+                    tag_type = 'LOCATION'
+                token_str = tokens[j]
+            elif tag in [2, 4, 6]:
+                token_str += ' ' + tokens[j]
+        if token_str:
+            if tag_type not in tag_dict:
+                tag_dict[tag_type] = []
+            tag_dict[tag_type].append(token_str)
+        for j, tag_type in enumerate(tag_dict):
+            new_l = []
+            for el in tag_dict[tag_type]:
+                if el not in new_l:
+                    new_l.append(el)
+            tag_dict[tag_type] = new_l
+        input_text = ' '.join(tokens)
+        target_l = []
+        target_text = ''
+        for j, tag_type in enumerate(tag_dict):
+            target_l.append(f'{tag_type}: {", ".join(tag_dict[tag_type])}')
+        target_text = ' | '.join(target_l)
+        input_text = input_text.strip()
+        target_text = target_text.replace('PERSON: ', 'Kişi: ').replace('LOCATION: ', 'Yer: ').replace('ORGANIZATION: ', 'Kuruluş: ').strip()
+        if not target_text:
+            target_text = 'Bulunamadı.'
+        input_texts.append(input_text)
+        target_texts.append(target_text)
+    return {'input_text': input_texts, 'target_text': target_texts}
 
 def preprocess_xtreme_ner(examples):
     tokens = examples['tokens']
