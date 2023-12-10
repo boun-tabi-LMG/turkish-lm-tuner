544de500b009d2ef9597355a250f4662132b24a1
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-12-10 13:47:35 +0300

    add compute lengths bash script

---
+++ compute_lengths.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=compute_lengths
+#SBATCH --mail-type=ALL
+#SBATCH --output=outs/%j.log
+#SBATCH --mail-user=username@gmail.com
+#SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
+#SBATCH --container-mounts /stratch/bounllm/:/stratch/bounllm/
+#SBATCH --time=7-00:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+
+echo $1
+echo $2
+
+source /opt/python3/venv/base/bin/activate
+
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+cd ~/t5-tuner
+pip install -r requirements.txt
+python src/compute_lengths.py --dataset_name $1 --task $2 #--no-preprocess
