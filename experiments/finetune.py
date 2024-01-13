90fac30cb60f2e2217ed8ac6f3923ba6c8e6099c
Gökçe Uludoğan <gokceuludogan@gmail.com>
2024-01-13 22:49:47 +0300

    Fix trainer for classification initialization

--- experiments/finetune.py
+++ experiments/finetune.py
@@ -92,7 +92,7 @@ def main(cfg: DictConfig):
         model_trainer = TrainerForConditionalGeneration(model_name, task, training_params, optimizer_params, model_save_path, max_input_length, max_target_length, postprocess_fn)
     elif task_format == 'classification':
         logger.info("******Classification Mode******")
-        model_trainer = TrainerForClassification(model_name, task, training_params, optimizer_params, model_save_path, num_labels)
+        model_trainer = TrainerForClassification(model_name, task, training_params, optimizer_params, model_save_path, num_labels, postprocess_fn)
 
     trainer, model = model_trainer.train_and_evaluate(train_dataset, eval_dataset, test_dataset)
 
