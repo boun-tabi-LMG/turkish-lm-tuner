f689045b98afcce651bbb3b8b6b61527dea92a73
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-01-13 22:53:38 +0300

    update squad function

--- turkish_lm_tuner/evaluator.py
+++ turkish_lm_tuner/evaluator.py
@@ -146,7 +146,7 @@ class EvaluatorForConditionalGeneration(BaseEvaluator):
         if isinstance(eval_preds, tuple) and len(eval_preds) == 2:
             preds, labels = eval_preds
             inputs = None
-        elif isinstance(eval_preds, EvalPrediction):
+        elif isinstance(eval_preds, EvalPrediction): # qa uses
             preds, labels, inputs = eval_preds.predictions, eval_preds.label_ids, eval_preds.inputs
         else:
             preds, labels, inputs = eval_preds
