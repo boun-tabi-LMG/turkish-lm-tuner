cc611378c37ec32818507fd569f3e21c9938f2a7
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-01-20 16:21:46 +0300

    add classification functions for pos task

--- turkish_lm_tuner/tr_datasets.py
+++ turkish_lm_tuner/tr_datasets.py
@@ -1,12 +1,12 @@
-import datasets 
+import datasets
 import sys
-import json 
-import re 
+import json
+import re
 
-from pathlib import Path 
+from pathlib import Path
 
 class BaseDataset:
-    DATASET_NAME = None 
+    DATASET_NAME = None
     DATASET_INFO = None
     def __init__(self, dataset_name=None, dataset_info=None):
         if dataset_name is not None:
@@ -25,10 +25,10 @@ class BaseDataset:
             return datasets.load_dataset(self.dataset_info, split=split)
         else:
             raise NotImplementedError
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["text"], "label": examples["label"]}
-            
+
     def postprocess_data(self, examples):
         return [ex.strip() if isinstance(ex, str) else ex for ex in examples]
 
@@ -41,84 +41,84 @@ class BaseDataset:
         return dedup_dataset
 
 
-class TRNewsDataset(BaseDataset): 
+class TRNewsDataset(BaseDataset):
     DATASET_NAME = "tr_news"
     DATASET_INFO = "batubayk/TR-News"
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["content"], "target_text": examples["abstract"]}
 
 class MLSumDataset(BaseDataset):
     DATASET_NAME = "mlsum"
     DATASET_INFO = ("mlsum", "tu")
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["text"], "target_text": examples["summary"]}
 
-class TRNewsTitleDataset(BaseDataset): 
+class TRNewsTitleDataset(BaseDataset):
     DATASET_NAME = "tr_news"
     DATASET_INFO = "batubayk/TR-News"
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["content"], "target_text": examples["title"]}
 
 class MLSumTitleDataset(BaseDataset):
     DATASET_NAME = "mlsum"
     DATASET_INFO = ("mlsum", "tu")
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["text"], "target_text": examples["title"]}
-    
+
 class CombinedNewsDataset(TRNewsDataset):
     DATASET_NAME = "combined_news"
     DATASET_INFO = ["tr_news", "mlsum"]
-    
+
     def load_dataset(self, split=None):
         trnews = TRNewsDataset().load_dataset(split)
         mlsum = MLSumDataset().load_dataset(split)
         mlsum = mlsum.rename_column("text", "content")
         mlsum = mlsum.rename_column("summary", "abstract")
         if split is not None:
-            return datasets.concatenate_datasets([trnews, mlsum]) 
+            return datasets.concatenate_datasets([trnews, mlsum])
         else:
             combined_data = {}
             for key in trnews.keys():
-                combined_data[key] = datasets.concatenate_datasets([trnews[key], mlsum[key]]) 
+                combined_data[key] = datasets.concatenate_datasets([trnews[key], mlsum[key]])
             # Returns DatasetDict object which is compatible with other datasets but takes a lot of time
             # return datasets.Dataset.from_dict(combined_data)
             # Returns a dictionary of DatasetDicts which is not compatible with other datasets but is faster
-            return combined_data 
+            return combined_data
 
 class CombinedNewsTitleDataset(TRNewsDataset):
     DATASET_NAME = "combined_news"
     DATASET_INFO = ["tr_news", "mlsum"]
-    
+
     def load_dataset(self, split=None):
         trnews = TRNewsDataset().load_dataset(split)
         mlsum = MLSumDataset().load_dataset(split)
         mlsum = mlsum.rename_column("text", "content")
         if split is not None:
-            return datasets.concatenate_datasets([trnews, mlsum]) 
+            return datasets.concatenate_datasets([trnews, mlsum])
         else:
             combined_data = {}
             for key in trnews.keys():
-                combined_data[key] = datasets.concatenate_datasets([trnews[key], mlsum[key]]) 
+                combined_data[key] = datasets.concatenate_datasets([trnews[key], mlsum[key]])
             # Returns DatasetDict object which is compatible with other datasets but takes a lot of time
             # return datasets.Dataset.from_dict(combined_data)
             # Returns a dictionary of DatasetDicts which is not compatible with other datasets but is faster
-            return combined_data 
-        
+            return combined_data
+
 class OpenSubtitlesDataset(BaseDataset):
     DATASET_NAME = "opensubtitles"
     DATASET_INFO = "mrbesher/tr-paraphrase-opensubtitles2018"
 
     def preprocess_data(self, examples):
         return {"input_text": examples["src"], "target_text": examples["tgt"]}
-    
+
 class TatoebaDataset(BaseDataset):
     DATASET_NAME = "tatoeba"
     DATASET_INFO = "mrbesher/tr-paraphrase-tatoeba"
-    
+
     def preprocess_data(self, examples):
         return {"input_text": examples["src"], "target_text": examples["tgt"]}
 
@@ -128,7 +128,7 @@ class TEDDataset(BaseDataset):
 
     def preprocess_data(self, examples):
         return {"input_text": examples["src"], "target_text": examples["tgt"]}
- 
+
 class LocalDataset(BaseDataset):
 
     def __init__(self, dataset_loc):
@@ -137,8 +137,8 @@ class LocalDataset(BaseDataset):
 
     def load_dataset(self, split=None, **kwargs):
         return datasets.load_dataset(self.dataset_loc, data_files=self.dataset_info, split=split, **kwargs)
-    
-    
+
+
 class STSb_TRDataset(LocalDataset):
     DATASET_NAME = "stsb_tr"
     DATASET_INFO = {'train': 'stsb_tr_train.tsv', 'test': 'stsb_tr_test.tsv', 'validation': 'stsb_tr_dev.tsv'}
@@ -150,7 +150,7 @@ class STSb_TRDataset(LocalDataset):
             return {"input_text": input, "label": examples["score"]}
         output = [str(ex) for ex in examples["score"]]
         return {"input_text": input, "target_text": output}
-    
+
     def postprocess_data(self, examples):
         def convert_sts_label(label):
             try:
@@ -167,15 +167,15 @@ class NLI_TRDataset(BaseDataset):
         # dataset_name is either "nli_tr", "snli_tr" or "multinli_tr"
         super().__init__(dataset_name)
         self.dataset_info = (self.DATASET_INFO[0], dataset_name)
-    
+
     def load_dataset(self, split=None):
         if self.dataset_name == "nli_tr":
             if split == "train":
                 mnli_tr = NLI_TRDataset("multinli_tr").load_dataset(split)
                 snli_tr = NLI_TRDataset("snli_tr").load_dataset(split)
-                dataset = datasets.concatenate_datasets([mnli_tr, snli_tr]) 
+                dataset = datasets.concatenate_datasets([mnli_tr, snli_tr])
             else:
-                dataset = NLI_TRDataset("snli_tr").load_dataset(split) 
+                dataset = NLI_TRDataset("snli_tr").load_dataset(split)
         elif self.dataset_name == 'snli_tr':
             dataset = super().load_dataset(split)
             """elif split is None:
@@ -185,16 +185,16 @@ class NLI_TRDataset(BaseDataset):
         else:
             dataset = super().load_dataset(split)
         return dataset.filter(lambda example: example["label"] != -1)
-        
+
     def preprocess_data(self, examples, skip_output_processing=False):
-        
+
         input = [f"hipotez: {examples['hypothesis'][i]} önerme: {examples['premise'][i]}" for i in range(len(examples["premise"]))]
         # If used with the classification mode, skip the output processing
         if skip_output_processing:
             return {"input_text": input, "label": examples["label"]}
         output = [NLI_TRDataset.IN_LABEL_DICT[ex] for ex in examples["label"]]
         return {"input_text": input, "target_text": output}
-    
+
     def postprocess_data(self, examples):
         return [NLI_TRDataset.OUT_LABEL_DICT.get(ex.strip(), -1) for ex in examples]
 
@@ -214,7 +214,7 @@ class ExamsDataset(QADataset):
             return super().load_dataset(split='validation')
         else:
             return super().load_dataset(split)
-        
+
     def preprocess_data(self, examples):
         input_texts, target_texts = [], []
         for question, answer_key in zip(examples["question"], examples["answerKey"]):
@@ -231,14 +231,14 @@ class ExamsDataset(QADataset):
             input_texts.append(question_str)
             target_texts.append(answer)
         return {"input_text": input_texts, 'target_text': target_texts}
-    
+
 class TQUADDataset(LocalDataset, QADataset):
     DATASET_NAME = "tquad"
     DATASET_INFO = {'train': 'train-v0.1.json', 'test': 'dev-v0.1.json'}
 
     def load_dataset(self, split=None):
         return super().load_dataset(split, field='data')
-    
+
     def preprocess_data(self, examples):
         input_texts, target_texts = [], []
         for paragraphs in examples['paragraphs']:
@@ -254,10 +254,10 @@ class TQUADDataset(LocalDataset, QADataset):
                     input_texts.append(input_text)
                     target_texts.append(target_text)
         return {"input_text": input_texts, "target_text": target_texts}
-   
+
 class MKQADataset(QADataset):
     DATASET_NAME = "mkqa"
-    DATASET_INFO = "mkqa"    
+    DATASET_INFO = "mkqa"
 
     def load_dataset(self, split='train'):
         return datasets.load_dataset('mkqa', split='train')
@@ -296,6 +296,7 @@ class NERDataset(BaseDataset):
         5: "B-ORGANIZATION",
         6: "I-ORGANIZATION",
     }
+
     def preprocess_data(self, examples, tokenizer):
         tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
         inputs = []
@@ -316,7 +317,7 @@ class NERDataset(BaseDataset):
             inputs.append(" ".join(examples["tokens"][i]).strip())
         tokenized_inputs["labels"] = labels
         return tokenized_inputs
-    
+
     def postprocess_labels(self, examples):
         preds, labels = examples
         true_predictions = [
@@ -328,7 +329,7 @@ class NERDataset(BaseDataset):
             for prediction, label in zip(preds, labels)
         ]
         return true_predictions, true_labels
-    
+
     def postprocess_text(self, examples, inputs):
         labels = []
         for example, input_t in zip(examples, inputs):
@@ -371,9 +372,9 @@ class NERDataset(BaseDataset):
     def postprocess_data(self, examples, inputs=None):
         if inputs is None:
            return self.postprocess_labels(examples)
-        else: 
+        else:
             return self.postprocess_text(examples, inputs)
-        
+
 class WikiANNDataset(NERDataset):
     DATASET_NAME = "wikiann"
     DATASET_INFO = ("wikiann", "tr")
@@ -416,7 +417,7 @@ class WikiANNDataset(NERDataset):
             input_texts.append(input_text)
             target_texts.append(target_text)
         return {'input_text': input_texts, 'target_text': target_texts}
-    
+
 class MilliyetNERDataset(LocalDataset,NERDataset):
     DATASET_NAME = "milliyet_ner"
     DATASET_INFO = {'train': 'train.json', 'test': 'test.json', 'validation': 'dev.json'}
@@ -449,7 +450,7 @@ class MilliyetNERDataset(LocalDataset,NERDataset):
                     with open(data_file, 'a', encoding='utf-8') as f:
                         f.write(json.dumps(el) + '\n')
         return super().load_dataset(split)
- 
+
     def preprocess_data(self, examples, skip_output_processing=False, tokenizer=None):
         if skip_output_processing:
             return super().preprocess_data(examples, tokenizer)
@@ -496,17 +497,18 @@ class MilliyetNERDataset(LocalDataset,NERDataset):
             input_texts.append(input_text)
             target_texts.append(target_text)
         return {'input_text': input_texts, 'target_text': target_texts}
-    
+
 class POSDataset(LocalDataset):
     DATASET_NAME = "pos"
     DATASET_INFO = {'train': 'train.json', 'test': 'test.json', 'validation': 'dev.json'}
     POS_TR_DICT = { "ADP": "edat", "AUX": "yardımcı", "PRON": "zamir", "NOUN": "isim", "PROPN": "özel", "INTJ": "ünlem", "PART": "tanımcık", "CCONJ": "eşgüdümlü", "VERB": "fiil", "SYM": "sembol", "DET": "belirteç", "ADV": "zarf", "ADJ": "sıfat", "X": "diğer", "SCONJ": "yantümce", "NUM": "sayı", "PUNCT": "noktalama" }
     POS_INT_DICT = {"edat": 0, "yardımcı": 1, "zamir": 2, "isim": 3, "özel": 4, "ünlem": 5, "tanımcık": 6, "eşgüdümlü": 7, "fiil": 8, "sembol": 9, "belirteç": 10, "zarf": 11, "sıfat": 12, "diğer": 13, "yantümce": 14, "sayı": 15, "noktalama": 16}
+    label_mapping = {v: k for k, v in POS_INT_DICT.items()}
 
     def __init__(self, dataset_loc=None, dataset_raw_info=None):
         super().__init__(dataset_loc)
         self.DATASET_RAW_INFO = dataset_raw_info
-    
+
     def load_dataset(self, split=None):
         md_pattern = re.compile('^# (.+?) = (.+?)$')
         annotation_pattern = re.compile('(.+\t){9}.+')
@@ -556,8 +558,31 @@ class POSDataset(LocalDataset):
                                 f.write(json.dumps(d_t) + '\n')
                             break
         return super().load_dataset(split)
-    
-    def preprocess_data(self, examples):
+
+    def preprocess_classification(self, examples, tokenizer):
+        tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+        inputs = []
+        labels = []
+        for i, label in enumerate(examples[f"tags"]):
+            word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+            previous_word_idx = None
+            label_ids = []
+            for word_idx in word_ids:  # Set the special tokens to -100.
+                if word_idx is None:
+                    label_ids.append(-100)
+                elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                    label_ids.append(POSDataset.POS_INT_DICT[label[word_idx]] if isinstance(label[word_idx], str) else label[word_idx])
+                else:
+                    label_ids.append(-100)
+                previous_word_idx = word_idx
+            labels.append(label_ids)
+            inputs.append(" ".join(examples["tokens"][i]).strip())
+        tokenized_inputs["labels"] = labels
+        return tokenized_inputs
+
+    def preprocess_data(self, examples, skip_output_processing=False, tokenizer=None):
+        if skip_output_processing:
+            return POSDataset.preprocess_classification(examples, tokenizer)
         input_texts, target_texts = [], []
         for ids, tokens, tags in zip(examples['ids'], examples['tokens'], examples['tags']):
             tag_l = []
@@ -577,8 +602,20 @@ class POSDataset(LocalDataset):
             input_texts.append(' '.join(tokens))
             target_texts.append(output)
         return {"input_text": input_texts, "target_text": target_texts}
-    
-    def postprocess_data(self, examples, inputs):
+
+    def postprocess_labels(self, examples):
+        preds, labels = examples
+        true_predictions = [
+            [POSDataset.label_mapping[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(preds, labels)
+        ]
+        true_labels = [
+            [POSDataset.label_mapping[l] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(preds, labels)
+        ]
+        return true_predictions, true_labels
+
+    def postprocess_text(self, examples, inputs):
         labels = []
         for input_t, example in zip(inputs, examples):
             example = example.strip()
@@ -593,8 +630,14 @@ class POSDataset(LocalDataset):
                 if label != '':
                     label_l[i] = 'B-' + label
             labels.append(label_l)
-        return labels    
-    
+        return labels
+
+    def postprocess_data(self, examples, inputs=None):
+        if inputs is None:
+           return self.postprocess_labels(examples)
+        else:
+            return self.postprocess_text(examples, inputs)
+
 class UDBOUNDataset(POSDataset):
     DATASET_NAME = "boun"
     DATASET_RAW_INFO =  {'train': 'tr_boun-ud-train.conllu', 'test': 'tr_boun-ud-test.conllu', 'validation': 'tr_boun-ud-dev.conllu'}
@@ -621,47 +664,47 @@ class ClassificationDataset(BaseDataset):
         return [self.OUT_LABEL_DICT.get(ex.strip(), -1) for ex in examples]
 
     def load_dataset(self, split=None):
-        return super().load_dataset(split)    
-         
+        return super().load_dataset(split)
+
 class TTC4900Dataset(ClassificationDataset):
     DATASET_NAME = "ttc4900"
-    DATASET_INFO = "ttc4900" 
+    DATASET_INFO = "ttc4900"
     IN_LABEL_DICT = {0: "siyaset", 1: "dünya", 2: "ekonomi", 3: "kültür", 4: "sağlık", 5: "spor", 6: "teknoloji"}
 
     def __init__(self, dataset_name=None):
         super().__init__(dataset_name)
 
     def preprocess_data(self, examples, skip_output_processing=False):
-        # If used with the classification mode, don't process the output 
+        # If used with the classification mode, don't process the output
         if skip_output_processing:
             return {"input_text": examples["text"], "label": examples["category"]}
         output = [self.IN_LABEL_DICT[ex] for ex in examples["category"]]
         return {"input_text": examples["text"], "target_text": output}
 
     def load_dataset(self, split=None):
-        dataset = super().load_dataset(split)   
+        dataset = super().load_dataset(split)
         print("Deduplicating data")
-        return super().deduplicate_data(dataset, "text")  
+        return super().deduplicate_data(dataset, "text")
 
 class ProductDataset(ClassificationDataset):
     DATASET_NAME = "turkish_product_reviews"
-    DATASET_INFO = "turkish_product_reviews" 
+    DATASET_INFO = "turkish_product_reviews"
     IN_LABEL_DICT = {0: "negatif", 1: "pozitif"}
 
     def __init__(self, dataset_name=None):
         super().__init__(dataset_name)
 
     def preprocess_data(self, examples, skip_output_processing=False):
-        # If used with the classification mode, don't process the output 
+        # If used with the classification mode, don't process the output
         if skip_output_processing:
             return {"input_text": examples["sentence"], "label": examples["sentiment"]}
         output = [self.IN_LABEL_DICT[ex] for ex in examples["sentiment"]]
         return {"input_text": examples["sentence"], "target_text": output}
-    
+
     def load_dataset(self, split=None):
-        dataset = super().load_dataset(split)   
+        dataset = super().load_dataset(split)
         print("Deduplicating data")
-        return super().deduplicate_data(dataset, "sentence")  
+        return super().deduplicate_data(dataset, "sentence")
 
 class SentimentTweetDataset(ClassificationDataset):
     DATASET_NAME = "sentiment_tweet"
@@ -673,12 +716,12 @@ class SentimentTweetDataset(ClassificationDataset):
         self.dataset_loc = dataset_loc
 
     def preprocess_data(self, examples, skip_output_processing=False):
-        # If used with the classification mode, don't process the output 
+        # If used with the classification mode, don't process the output
         if skip_output_processing:
             return {"input_text": examples["text"], "label": examples["label"]}
         output = [self.IN_LABEL_DICT[ex] for ex in examples["label"]]
         return {"input_text": examples["text"], "target_text": output}
-    
+
     def postprocess_data(self, examples):
         return [self.OUT_LABEL_DICT.get(ex.strip(), -1) for ex in examples]
 
@@ -686,7 +729,7 @@ class SentimentTweetDataset(ClassificationDataset):
         dataset = LocalDataset.load_dataset(self, split)
         #dataset = datasets.load_dataset(self.dataset_loc, data_files=self.dataset_info, split=split)
         print("Deduplicating data")
-        return super().deduplicate_data(dataset, "text")  
+        return super().deduplicate_data(dataset, "text")
 
 DATASET_MAPPING_NAMES = [
         ("tr_news", "TRNewsDataset"),
