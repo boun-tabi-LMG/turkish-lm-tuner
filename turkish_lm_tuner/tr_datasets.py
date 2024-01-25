7c3b217867676732ed5ed078e7fa9d6e10f1e702
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-01-25 12:32:43 +0300

    fix 0 to O

--- turkish_lm_tuner/tr_datasets.py
+++ turkish_lm_tuner/tr_datasets.py
@@ -620,7 +620,7 @@ class POSDataset(LocalDataset):
             example = example.strip()
             input_tokens = input_t.split(' ')
             tokens = example.split(' ')
-            label_l = ['0' for i in range(len(input_tokens))]
+            label_l = ['O' for i in range(len(input_tokens))]
             for i, token in enumerate(tokens):
                 if i >= len(label_l):
                     break
